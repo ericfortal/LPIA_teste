aedf2b610e0defc74a375e68a3b78fe10c8ccd5f
Eric Viana <152749608+ericfortal@users.noreply.github.com>
2024-11-16 20:21:46 -0300

    final1resolucao2

---
+++ atividade2.py
@@ -0,0 +1,12 @@
+# Atividade 02:
+# Verificar Igualdade de Strings:
+# Peça ao usuário duas palavras e use operadores
+# de comparação para verificar se elas são iguais.
+
+palavra1 = input("Digite a primeira palavra: ").lower()
+palavra2 = input("Digite a segunda palavra: ").lower()
+
+if palavra1 == palavra2:
+    print("As palavras são iguais!")
+else:
+    print("As palavras são diferentes!")
