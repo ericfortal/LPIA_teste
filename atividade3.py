29cbdb95f222e201ee62e83d980bab1501b6e4e5
Eric Viana <152749608+ericfortal@users.noreply.github.com>
2024-11-16 23:31:44 +0000

    executandoatividade

---
+++ atividade3.py
@@ -0,0 +1,12 @@
+# Atividade 03:
+# Verificação de Maioridade e Habilitação:
+# Crie um programa que peça a idade do usuário e se ele possui habilitação
+# (sim ou não). Use operadores lógicos para verificar se ele é maior de idade
+# (>= 18) e possui habilitação.
+
+idade = int(input("Digite sua idade:"))
+
+habilitacao - input("Você possui habilitação? Sim ou não?").strip().lower()
+
+if idade >= 18 && habilitacao == "sim":
+    print("Você é maior de idade e habilitado!")
